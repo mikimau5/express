11a0303f98e6165cc25b360bad98490d74fabd73
Miki <alerpace@o2.pl>
2019-06-03 13:34:00 +0200

    news filter

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: '',
+  db: 'mongodb+srv://admin:w3wMdqoh35wkSnaG@cluster0-oqepd.mongodb.net/test?retryWrites=true&w=majority',
   keySession: ['TWOJKLUCZ'],
   maxAgeSession: 24 * 60 * 60 * 1000
 }
